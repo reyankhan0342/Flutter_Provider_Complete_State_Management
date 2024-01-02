3bc532ef064066372fb04b2daee4f544219a2a5d
reyankhan0342 <sashaandoo0342@gmail.com>
2024-01-02 12:50:09 +0500

    Flutter provider state Mangement

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_provider_state_managment/screens/examples/favourite_example/favourite_screen.dart';
+import 'package:flutter_provider_state_managment/provider/auth_provider.dart';
+import 'package:flutter_provider_state_managment/provider/count_provider.dart';
+import 'package:flutter_provider_state_managment/provider/example_one_provider.dart';
+import 'package:flutter_provider_state_managment/provider/example_two_provider.dart';
+import 'package:flutter_provider_state_managment/screens/examples/value_notifier_listner.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => CountProvider()),
+          ChangeNotifierProvider(create: (_) => ExampleOneProvider()),
+          ChangeNotifierProvider(create: (_) => ExampleTwoProvider()),
+          ChangeNotifierProvider(create: (_) =>  AuthProvider()),
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const FavouriteScreen(),
+        ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home:  ValueNotifierListener(),
+        )
     );
   }
 }
-
