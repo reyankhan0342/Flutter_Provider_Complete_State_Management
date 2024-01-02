fd660032bdfb5ad2124115ca80f254b7d2d52076
reyankhan0342 <sashaandoo0342@gmail.com>
2024-01-02 12:52:22 +0500

    remove debug show baner

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
 
         ],
         child: MaterialApp(
+          debugShowCheckedModeBanner: false,
           title: 'Flutter Demo',
           theme: ThemeData(
             primarySwatch: Colors.blue,
